fb0d426bc4826bb5ac03cfec0a9b735231bfe67f
chicogui <franciscoguilherme.v@gmail.com>
2020-01-23 13:54:35 -0300

    continuação;

--- javascript/toDoList/js_functions.js
+++ javascript/toDoList/js_functions.js
@@ -12,25 +12,35 @@
 
 				for (tarefa of tarefas) {
 					var todoElement = document.createElement('li');
+					todoElement.setAttribute('id', 'item_tarefa');
 					var todoText = document.createTextNode(tarefa);
 					
 
-					var linkElement = document.createElement('a');
-					linkElement.setAttribute('href', '#');
-					linkElement.setAttribute('class', 'btn btn-outline-danger');
+					var remover_tarefa = document.createElement('a');
+					remover_tarefa.setAttribute('href', '#');
+					remover_tarefa.setAttribute('class', 'btn btn-outline-danger excluir_tarefa');
+
+
+					var arquivar_tarefa = document.createElement('a');
+					arquivar_tarefa.setAttribute('href', '#');
+					arquivar_tarefa.setAttribute('class', 'btn btn-outline-success arquivar_tarefa');
 
 					var pos = tarefas.indexOf(tarefa);
-					linkElement.setAttribute('onclick', 'deleteTodo(' + pos + ')');
+					remover_tarefa.setAttribute('onclick', 'deleteTodo(' + pos + ')');
+					arquivar_tarefa.setAttribute('onclick', 'arquivarTodo(' + pos + ')');
 
 					
 
-					var linkText = document.createTextNode('Excluir');
+					var linkExcluir = document.createTextNode('Excluir');
+					var linkArquivar = document.createTextNode('feito');
 
-					linkElement.appendChild(linkText);
+					remover_tarefa.appendChild(linkExcluir);
+					arquivar_tarefa.appendChild(linkArquivar);
 
 					todoElement.appendChild(todoText);
-					todoElement.appendChild(linkElement);
-
+					todoElement.appendChild(arquivar_tarefa);
+					todoElement.appendChild(remover_tarefa);
+					
 					listElement.appendChild(todoElement);
 				}
 
@@ -55,9 +65,16 @@
 			buttonElement.onclick = addTodo;
 
 			function deleteTodo(pos) {
+				alert("esta tarefa será exluida");
 				tarefas.splice(pos, 1);
 				renderTodos();
 				saveToStorage();
+			}
+
+			function arquivarTodo(pos) {
+				alert("esta tarefa será arqivada");
+
+				
 			}
 
 			function saveToStorage(){
